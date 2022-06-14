dcb4922c7f58a033e73725e0e7c179085157b1e3
Shamus <shamus.hackett@gmail.com>
2022-06-13 20:40:56 -0400

    finished

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
-// TODO: add code here
+window.addEventListener("load", function(){
+    fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function(response){
+        response.json().then(function(json) {
+            const sortObject = obj => {
+                const arr = json.keys(obj).map(el => {
+                   return obj[el];
+                });
+                arr.sort((a, b) => {
+                   return a - b;
+                });
+                return arr;
+             };
+             console.log(sortObject(json));
+
+               const container = document.getElementById("container"); 
+           let html = '';
+           for(let astronaut of json){
+               html += `
+               <div class="astronaut">
+                   <div class="bio">
+                       <h3>${astronaut.firstName} ${astronaut.lastName}</h3>
+                            <ul>
+                            <li>Hours in Space: ${astronaut.hoursInSpace}</li>
+                            <li>Active: ${astronaut.active}</li>
+                            <li>Skills: ${astronaut.skills}</li>
+                            </ul>
+                    </div>
+                    <img class="avatar" src="${astronaut.picture}" alt = "${astronaut.firstName} ${astronaut.lastName}">
+                </div>
+                `
+           }
+           container.innerHTML = html;
+        });
+     });
+})
+
